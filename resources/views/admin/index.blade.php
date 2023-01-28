923f30dde0082a4603a3ff09b087212769550b7b
hafsakzoun <118023551+hafsakzoun@users.noreply.github.com>
2023-01-28 14:53:45 +0100

    Co-authored-by: yamina118 <yamina118@users.noreply.github.com> Co-authored-by: kaddourferdaous <kaddourferdaous@users.noreply.github.com> Co-authored-by: ImaneDouas <ImaneDouas@users.noreply.github.com>

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,9 +1,9 @@
-@extends('layouts.admin')
+@extends('layouts.admina')
 
 @section('content')
         <div class="card">
             <div class="card-body">
-                <h1>Furniting</h1>
+                <h1>hafsa</h1>
             </div>
         </div>
 @endsection
--- resources/views/layouts/admina.blade.php
+++ resources/views/layouts/admina.blade.php
@@ -1,16 +1,17 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getlocale()) }}">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
    <!-- CSRF TOKEN-->
-    <meta name="csrf-token content" content="{{ csrf_token() }}">
-    <title>{{ config('app.name' , 'laravel') }}</title>
+   <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Furnishings @yield('title')</title>
 
     <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
    
-    <link href="{{ asset('admin/css/material-dashboard.css) }}" rel="stylesheet">
+    <link href="{{ asset('admin/css/material-dashboard.css') }}" rel="stylesheet">
+
 
 </head>
 <body>
