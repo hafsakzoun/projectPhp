583b1e802330b8343394fc1e25bed3e87062dc4f
ImaneDouas <118218912+ImaneDouas@users.noreply.github.com>
2023-01-28 14:54:46 +0100

    update

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -14,6 +14,10 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
 
+ <!--cart-->
+ <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+
+
     <style type="text/css">
         a{
             text-decoration: none;
