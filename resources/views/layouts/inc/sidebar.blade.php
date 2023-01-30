3144ff92bc0d79d378a8205715f4902a8dc706fb
hafsakzoun <118023551+hafsakzoun@users.noreply.github.com>
2023-01-30 18:28:30 +0100

    hafsa4

--- resources/views/layouts/inc/sidebar.blade.php
+++ resources/views/layouts/inc/sidebar.blade.php
@@ -10,21 +10,21 @@
       <div class="sidebar-wrapper">
         <ul class="nav">
           <li class="nav-item active  ">
-            <a class="nav-link" href="./dashboard.html">
+            <a class="nav-link" href="#">
               <i class="material-icons">dashboard</i>
               <p>Dashboard</p>
             </a>
           </li>
           <li class="nav-item ">
-            <a class="nav-link" href="/categories">
+            <a class="nav-link" href="{{url('categories')}}">
               <i class="material-icons">person</i>
               <p>categories</p>
             </a>
           </li>
           <li class="nav-item ">
-            <a class="nav-link" href="./tables.html">
+            <a class="nav-link" href="#">
               <i class="material-icons">content_paste</i>
-              <p>Table List</p>
+              <p>Products</p>
             </a>
           </li>
         </ul>
